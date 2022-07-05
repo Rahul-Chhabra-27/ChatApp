7a5604016cdc7347f951a7cbfc74dca2c1ca724d
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-07-05 17:36:38 +0530

    Web socket connection established

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const http = require('http');
+const socketio = require('socket.io');
 dotenv.config();
-const app = express();
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 app.use(express.static(path.join(__dirname, "../", "static")));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+io.on('connection',() => {
+    console.log('New websocket connection established..');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
