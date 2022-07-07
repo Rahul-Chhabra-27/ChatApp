99d49e190b2a6e2c6d7d94d463c29ebdbbbff9ee
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-07-07 18:51:35 +0530

    Chat.js file created that allow to communicate

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
-const http = require('http');
-const socketio = require('socket.io');
+const http = require("http");
+const socketio = require("socket.io");
 dotenv.config();
 
 const app = express();
@@ -10,8 +10,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 app.use(express.static(path.join(__dirname, "../", "static")));
 
-io.on('connection',() => {
-    console.log('New websocket connection established..');
+let count = 0;
+/// io.on() inkoves every time , when new user joins the client-server communication.
+io.on("connection", (server) => {
+  console.log("New websocket connection established..");
+  // Sending a request to a new client about updated count value... 
+  server.emit("updatedCount", count);
+  // Accepting the client request and doing accordingly....
+  server.on("incrementCount", () => {
+    count++;
+    // Sending a request back to all the clients that are connected and letting them know about the updated count value. 
+    io.emit("updatedCount", count);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
